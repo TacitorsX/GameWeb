3491db8878c5075107482e2ba0ccb4bb02c2c91c
Tácito Rodrigues <tacito.santos@dcomp.ufs.br>
2025-08-19 11:21:59 -0300

    Update scripts code
    Atualização dos códigos de html, css e javascript, adicionamento do código de switch para marcar a posição da barra e acréscimo da barra no css para o site.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const numberIndicator = document.querySelector('.numbers')
+const bar = document.getElementById("bar")
 const button = document.getElementById("button");
 const estado = {chutando: false}; /*controla se o jogador está chutando ou não*/
 const estado1 = {consegueChutar: true};/*Controla se o jogador conseguiu chutar*/
@@ -15,8 +16,48 @@ function pararChutar(){/*função pré-definida para utilizar no setTimeout*/
     estado1.consegueChutar = true;
 }
 
+//Marcador estilo barra
+function moverBarra(num){
+    switch(num){
+        case 0:
+            bar.style.transform = 'translate(-169.4px,-14px)'
+            break
+        case 1:
+            bar.style.transform = 'translate(-134.5px,-14px)'
+            break
+        case 2:
+            bar.style.transform = 'translate(-98px,-14px)'
+            break
+        case 3:
+            bar.style.transform = 'translate(-63px,-14px)'
+            break
+        case 4:
+            bar.style.transform = 'translate(-27.6px,-14px)'
+            break
+        case 5:
+            bar.style.transform = 'translate(7.5px,-14px)'
+            break
+        case 6:
+            bar.style.transform = 'translate(40.5px,-14px)'
+            break
+        case 7:
+            bar.style.transform = 'translate(75px,-14px)'
+            break
+        case 8:
+            bar.style.transform = 'translate(108px,-14px)'
+            break
+        case 9:
+            bar.style.transform = 'translate(141.7px,-14px)'
+            break
+        case 10:
+            bar.style.transform = 'translate(174.8px,-14px)'
+            break
+    }
+}
+
 function recurcao  (num,des,vel) {
     numberIndicator.textContent = String(num).padStart(2,0)
+    moverBarra(num)
     if (num >= 10){
         return setTimeout(recurcao, vel,num-1,true,vel)
     }

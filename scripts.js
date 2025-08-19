f9fc9a1e9b8158e6960d440285a0a66e0f4757c2
zlcarlos <luiz.dejesus@dcomp.ufs.br>
2025-08-18 23:28:51 -0300

    Transformação de código
    Utilizando funções já estabelecidas pelo colega, foi feita a transformação para o paradigma funcional.

--- scripts.js
+++ scripts.js
@@ -1,18 +1,18 @@
 const numberIndicator = document.querySelector('.numbers')
 const button = document.getElementById("button");
-var chutando = false
-var consegueChutar = true
+const estado = {chutando: false}; /*controla se o jogador está chutando ou não*/
+const estado1 = {consegueChutar: true};/*Controla se o jogador conseguiu chutar*/
 button.onclick = () => {
-    if (consegueChutar){
-        chutando = true
-        consegueChutar = false
-        setTimeout(pararChutar,1000)
+    if (estado1.consegueChutar){
+        estado.chutando = true;/*O jogador começou a chutar*/
+        estado1.consegueChutar = false;/*Jogador não chuta imediatamente após o chute anterior*/
+        setTimeout(pararChutar,1000)/*define quando parar o chute*/
     }
 }
 
-function pararChutar(){
-    chutando = false
-    consegueChutar = true
+function pararChutar(){/*função pré-definida para utilizar no setTimeout*/
+    estado.chutando = false;
+    estado1.consegueChutar = true;
 }
 
 function recurcao  (num,des,vel) {
@@ -20,7 +20,7 @@ function recurcao  (num,des,vel) {
     if (num >= 10){
         return setTimeout(recurcao, vel,num-1,true,vel)
     }
-    if (chutando){
+    if (estado.chutando){
         if (num <=2 && num >=0){
             if (vel - 20 >= 0){
                 return recurcao(num+1,false,vel-20)
